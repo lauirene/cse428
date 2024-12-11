991f775e8848454225f4714f3a81e0e750db87b3
Zhang038 <zhang038@purdue.edu>
2024-12-11 14:01:47 -0800

    write final output for sc hic

--- inference/main_worker.py
+++ inference/main_worker.py
@@ -210,6 +210,14 @@ def main_worker(args, input_pkl):
             write_pickle(current_dict,current_pkl)
             output_bigwig = os.path.join(output_dir,"HiCFoundation_pred_%s.bigWig"%key_word)
             array2bigwig(current_pkl,output_bigwig,resolution=resolution)
-    
+    elif args.task==5:
+        #scHi-C enhancement
+        output_path = os.path.join(output_dir,"HiCFoundation_sc_enhanced.pkl")
+        write_pickle(return_dict,output_path)
+        input_file = os.path.abspath(args.input)
+        extention_name = input_file.split('.')[-1]
+        output_file = os.path.join(output_dir,"HiCFoundation_sc_enhanced."+extention_name)
+        pkl2others(output_path, output_file,resolution,args.genome_id)
+
     print("Inference finished!")
     print("Enjoy your HiCFoundation results!")
