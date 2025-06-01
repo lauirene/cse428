fixed bug for training with multiple gpus -> set return tensors that are unnecessary to None | reduced memory usage: removed logging and hooks, added with torch.cuda.amp.autocast() for scaling with the NativeScaler

data_processing/finetune_dataset.py
@@ -116,7 +116,6 @@ class Finetune_Dataset(torch.utils.data.Dataset):
         return len(self.train_list)
     
     def convert_rgb(self,data_log,max_value):
-        print(f"DEBUG: max_value = {max_value}")
         if len(data_log.shape)==2:
             data_log = data_log[np.newaxis,:]
         data_red = np.ones(data_log.shape)
@@ -140,7 +139,7 @@ class Finetune_Dataset(torch.utils.data.Dataset):
         input_matrix = np.nan_to_num(input_matrix)
         input_matrix = input_matrix.astype(np.float32)
         input_matrix = np.log10(input_matrix+1)
-        print(f"DEBUG: input_matrix min={np.min(input_matrix)}, max={np.max(input_matrix)}")
+        # print(f"DEBUG: input_matrix min={np.min(input_matrix)}, max={np.max(input_matrix)}")
         max_value = np.max(input_matrix)
         input_matrix = self.convert_rgb(input_matrix,max_value)
         if self.transform:

finetune/train_epoch.py
@@ -13,8 +13,8 @@ from ops.Logger import MetricLogger,SmoothedValue
 import model.lr_sched as lr_sched
 from finetune.loss import configure_loss
 from ops.train_utils import list_to_device, to_value, create_image, torch_to_nparray, convert_gray_rgbimage
-attention_logs = []
-LOG_EVERY_N = 10  # log every 10 batches
+# attention_logs = []
+# LOG_EVERY_N = 10  # log every 10 batches
 
 
 def train_epoch(model, data_loader_train, optimizer, 
@@ -37,44 +37,45 @@ def train_epoch(model, data_loader_train, optimizer,
 
     num_iter = len(data_loader_train)
     for data_iter_step, train_data in enumerate(metric_logger.log_every(data_loader_train, print_freq, header)):
-        ## logging code ##################
-        if data_iter_step % LOG_EVERY_N == 0 and len(attention_logs) > 0:
-            output_path = os.path.join(args.output, f"attention_logs_epoch{epoch}_step{data_iter_step}.json")
-            with open(output_path, 'w') as f:
-                json.dump(attention_logs, f, indent=4)
-            print(f"[DEBUG] Saved attention logs to {output_path}")
-            attention_logs.clear()  # clear after saving
-        ##################################
+        # ## logging code ##################
+        # if data_iter_step % LOG_EVERY_N == 0 and len(attention_logs) > 0:
+        #     output_path = os.path.join(args.output, f"attention_logs_epoch{epoch}_step{data_iter_step}.json")
+        #     with open(output_path, 'w') as f:
+        #         json.dump(attention_logs, f, indent=4)
+        #     print(f"[DEBUG] Saved attention logs to {output_path}")
+        #     attention_logs.clear()  # clear after saving
+        # ##################################
         if data_iter_step % accum_iter == 0:
             lr_sched.adjust_learning_rate(optimizer, data_iter_step / len(data_loader_train) + epoch, args)
         input_matrix, total_count, target_matrix, embed_target, target_vector, sequence_data = list_to_device(train_data,device=device)
-        output_embedding, output_2d, output_1d = model(input_matrix, total_count, sequence_data)
+        with torch.cuda.amp.autocast():
+            output_embedding, output_2d, output_1d = model(input_matrix, total_count, sequence_data)
         
-        if embed_target is not None:
-            embedding_loss = criterion(output_embedding, embed_target)
-        else:
-            embedding_loss = 0
-        if target_matrix is not None:
-            if args.loss_type == 3:
-                print("using ssim")
-                if output_2d.dim() == 3:
-                    output_2d = output_2d.unsqueeze(1)
-                if target_matrix.dim() == 3:
-                    target_matrix = target_matrix.unsqueeze(1)
-                output_2d_loss = criterion(output_2d, target_matrix)
+            if embed_target is not None:
+                embedding_loss = criterion(output_embedding, embed_target)
             else:
-                print("not using ssim")
-                #flatten 2d matrix
-                output_2d = torch.flatten(output_2d, start_dim=1,end_dim=-1)
-                target_matrix_flatten = torch.flatten(target_matrix, start_dim=1,end_dim=-1)
-                output_2d_loss = criterion(output_2d, target_matrix_flatten)
-        else:
-            output_2d_loss = 0
-        if target_vector is not None:
-            output_1d_loss = criterion(output_1d, target_vector)
-        else:
-            output_1d_loss = 0
-        loss = embedding_loss + output_2d_loss + output_1d_loss #you can adjust the loss function based on your fine-tuning purpose
+                embedding_loss = 0
+            if target_matrix is not None:
+                if args.loss_type == 3:
+                    print("using ssim")
+                    if output_2d.dim() == 3:
+                        output_2d = output_2d.unsqueeze(1)
+                    if target_matrix.dim() == 3:
+                        target_matrix = target_matrix.unsqueeze(1)
+                    output_2d_loss = criterion(output_2d, target_matrix)
+                else:
+                    print("not using ssim")
+                    #flatten 2d matrix
+                    output_2d = torch.flatten(output_2d, start_dim=1,end_dim=-1)
+                    target_matrix_flatten = torch.flatten(target_matrix, start_dim=1,end_dim=-1)
+                    output_2d_loss = criterion(output_2d, target_matrix_flatten)
+            else:
+                output_2d_loss = 0
+            if target_vector is not None:
+                output_1d_loss = criterion(output_1d, target_vector)
+            else:
+                output_1d_loss = 0
+            loss = embedding_loss + output_2d_loss + output_1d_loss #you can adjust the loss function based on your fine-tuning purpose
         #typically, I think you should only finetune for one of the purposes
         metric_logger.update(loss=to_value(loss))
         metric_logger.update(embedding_loss=to_value(embedding_loss))

model/Finetune_Model_Head.py
@@ -211,18 +211,18 @@ class Finetune_Model_Head(nn.Module):
             decoder_output = self.forward_decoder(img,
                                                 total_count=total_count,
                                                 sequence_data=sequence_data)
-            submatrix_embedding = decoder_output[:,0,:]
+            # submatrix_embedding = decoder_output[:,0,:]
             pred_2d = self.decoder_map(decoder_output)
             pred_2d = pred_2d[:,self.num_additional_token:,:]
             pred_2d = self.unpatchify_channel(pred_2d,1)
-            patch_embedding = decoder_output[:,self.num_additional_token:,:]
-            num_patch_row = self.pos_embed_size[0]
-            num_patch_col = self.pos_embed_size[1]
-            pred_1d = patch_embedding.reshape(shape=(patch_embedding.shape[0], num_patch_row,num_patch_col,-1)) #average all columns
-            pred_1d = torch.mean(pred_1d,dim=2) #N, H, C
-            pred_1d = self.map_block(pred_1d) #N, H, D, where D is the output_dim
-            pred_1d = pred_1d.reshape(pred_1d.shape[0],-1) #N, H*D
-            return submatrix_embedding, pred_2d[:,0,:], pred_1d
+            # patch_embedding = decoder_output[:,self.num_additional_token:,:]
+            # num_patch_row = self.pos_embed_size[0]
+            # num_patch_col = self.pos_embed_size[1]
+            # pred_1d = patch_embedding.reshape(shape=(patch_embedding.shape[0], num_patch_row,num_patch_col,-1)) #average all columns
+            # pred_1d = torch.mean(pred_1d,dim=2) #N, H, C
+            # pred_1d = self.map_block(pred_1d) #N, H, D, where D is the output_dim
+            # pred_1d = pred_1d.reshape(pred_1d.shape[0],-1) #N, H*D
+            return None, pred_2d, None #submatrix_embedding, pred_2d[:,0,:], pred_1d
         elif self.task==1:
             #for reproducibility analysis
             decoder_output = self.forward_decoder(img,